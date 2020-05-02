2876bbd4d838af8dda7ce91b1f702688500c429a
Viacheslav Kaskov <Dr.Sfar@yandex.ru>
2020-05-02 18:18:54 +0300

    Закончен полный-'сырой' вариант логики dropdown-guests

--- src/components/__dropdown/dropdown.js
+++ src/components/__dropdown/dropdown.js
@@ -1,22 +1,28 @@
 class DropdownGuests {
   constructor () {
     this.dropdownShowInfo = document.querySelector('.dropdown__show-info');
+    this.dropdownAreaOfPlaceholder = document.querySelector('.dropdown__area-of-placeholder');
     this.choiceGuests = document.querySelector('.panel-of-choice');
-    this.outputAmountOfAdults = document.querySelector('.buttons-of-choice__amount-of-adults')
-    this.outputAmountOfChildren = document.querySelector('.buttons-of-choice__amount-of-children')
-    this.outputAmountOfBabyes = document.querySelector('.buttons-of-choice__amount-of-babyes')
-    this.dropdownAreaOfPlaceholder = document.querySelector('.dropdown__area-of-placeholder')
+    this.outputAmountOfAdults = document.querySelector('.buttons-of-choice__amount-of-adults');
+    this.outputAmountOfChildren = document.querySelector('.buttons-of-choice__amount-of-children');
+    this.outputAmountOfBabyes = document.querySelector('.buttons-of-choice__amount-of-babyes');
 
-    this.buttonRemoveAdults = document.querySelector('.button__remove-adults')
-    this.buttonAddAdults = document.querySelector('.button__add-adults')
-    this.buttonRemoveChildren = document.querySelector('.button__remove-children')
-    this.buttonAddChildren = document.querySelector('.button__add-children')
-    this.buttonRemoveBabyes = document.querySelector('.button__remove-babyes')
-    this.buttonAddBabyes = document.querySelector('.button__add-babyes')
+
+    this.buttonRemoveAdults = document.querySelector('.button__remove-adults');
+    this.buttonAddAdults = document.querySelector('.button__add-adults');
+    this.buttonRemoveChildren = document.querySelector('.button__remove-children');
+    this.buttonAddChildren = document.querySelector('.button__add-children');
+    this.buttonRemoveBabyes = document.querySelector('.button__remove-babyes');
+    this.buttonAddBabyes = document.querySelector('.button__add-babyes');
+    this.buttonClear = document.querySelector('.button__clear');
+    this.buttonApply = document.querySelector('.button__apply');
 
     this.amountOfAdults = 0;
     this.amountOfChildren = 0;
     this.amountOfBabyes = 0;
+    this.maxAdults = 16;
+    this.maxChildren = 16;
+    this.maxBabyes = 5;
     this.handlerOnTheButtons = true;
   }
   dropdownInit() {
@@ -29,6 +35,7 @@ class DropdownGuests {
       this.choiceGuests.classList.remove('hide');
       this.choiceGuests.classList.add('show');
 
+      this.hideAndShowButtonClear();
       if (this.handlerOnTheButtons) {
         this.choiceGuests.addEventListener('click', this.listenerOfButtons.bind(this))
         this.handlerOnTheButtons = false;
@@ -48,43 +55,92 @@ class DropdownGuests {
       this.outputAmountOfAdults.innerHTML = this.amountOfAdults;
       this.outputQuantityOfGuests();
       this.setAndRemoveDisabled(this.amountOfAdults, this.buttonRemoveAdults);
-      this.setMaxAmount(this.amountOfAdults, this.buttonAddAdults, 16);
+      this.setMaxAmount(this.amountOfAdults, this.buttonAddAdults, this.maxAdults);
     }
     if (target == 'button__remove-adults') {
-      this.amountOfAdults -= 1;
-      this.outputAmountOfAdults.innerHTML = this.amountOfAdults;
-      this.outputQuantityOfGuests();
-      this.setAndRemoveDisabled(this.amountOfAdults, this.buttonRemoveAdults);
-      this.setMaxAmount(this.amountOfAdults, this.buttonAddAdults, 16);
+      if ((this.amountOfAdults < 2 && (this.amountOfBabyes || this.amountOfChildren)) || this.amountOfAdults < 1) {
+        return null;
+      } else {
+        this.amountOfAdults -= 1;
+        this.outputAmountOfAdults.innerHTML = this.amountOfAdults;
+        this.outputQuantityOfGuests();
+        this.setAndRemoveDisabled(this.amountOfAdults, this.buttonRemoveAdults);
+        this.setMaxAmount(this.amountOfAdults, this.buttonAddAdults, this.maxAdults);
+      }
     }
     if (target == 'button__add-children') {
+      if (this.amountOfAdults === 0) {
+        this.amountOfAdults += 1;
+        this.outputAmountOfAdults.innerHTML = this.amountOfAdults;
+        this.outputQuantityOfGuests();
+        this.setAndRemoveDisabled(this.amountOfAdults, this.buttonRemoveAdults);
+      }
       this.amountOfChildren += 1;
       this.outputAmountOfChildren.innerHTML = this.amountOfChildren;
       this.outputQuantityOfGuests();
       this.setAndRemoveDisabled(this.amountOfChildren, this.buttonRemoveChildren);
-      this.setMaxAmount(this.amountOfChildren, this.buttonAddChildren, 16);
+      this.setMaxAmount(this.amountOfChildren, this.buttonAddChildren, this.maxChildren);
     }
     if (target == 'button__remove-children') {
-      this.amountOfChildren -= 1;
-      this.outputAmountOfChildren.innerHTML = this.amountOfChildren;
-      this.outputQuantityOfGuests();
-      this.setAndRemoveDisabled(this.amountOfChildren, this.buttonRemoveChildren);
-      this.setMaxAmount(this.amountOfChildren, this.buttonAddChildren, 16);
+      if (this.amountOfChildren < 1) {
+        return null;
+      } else {
+        this.amountOfChildren -= 1;
+        this.outputAmountOfChildren.innerHTML = this.amountOfChildren;
+        this.outputQuantityOfGuests();
+        this.setAndRemoveDisabled(this.amountOfChildren, this.buttonRemoveChildren);
+        this.setMaxAmount(this.amountOfChildren, this.buttonAddChildren, this.maxChildren);
+      }
     }
     if (target == 'button__add-babyes') {
+      if (this.amountOfAdults === 0) {
+        this.amountOfAdults += 1;
+        this.outputAmountOfAdults.innerHTML = this.amountOfAdults;
+        this.outputQuantityOfGuests();
+        this.setAndRemoveDisabled(this.amountOfAdults, this.buttonRemoveAdults);
+      }
       this.amountOfBabyes += 1;
       this.outputAmountOfBabyes.innerHTML = this.amountOfBabyes;
       this.outputQuantityOfGuests();
       this.setAndRemoveDisabled(this.amountOfBabyes, this.buttonRemoveBabyes);
-      this.setMaxAmount(this.amountOfBabyes, this.buttonAddBabyes, 5);
+      this.setMaxAmount(this.amountOfBabyes, this.buttonAddBabyes, this.maxBabyes);
     }
     if (target == 'button__remove-babyes') {
-      this.amountOfBabyes -= 1;
+      if (this.amountOfBabyes < 1) {
+        return null;
+      } else {
+        this.amountOfBabyes -= 1;
+        this.outputAmountOfBabyes.innerHTML = this.amountOfBabyes;
+        this.outputQuantityOfGuests();
+        this.setAndRemoveDisabled(this.amountOfBabyes, this.buttonRemoveBabyes);
+        this.setMaxAmount(this.amountOfBabyes, this.buttonAddBabyes, this.maxBabyes);
+      }
+    }
+    if (target == 'button__clear') {
+      this.amountOfAdults = 0;
+      this.amountOfChildren = 0;
+      this.amountOfBabyes = 0;
+
+      this.outputAmountOfAdults.innerHTML = this.amountOfAdults;
+      this.outputAmountOfChildren.innerHTML = this.amountOfChildren;
       this.outputAmountOfBabyes.innerHTML = this.amountOfBabyes;
+
+      this.outputQuantityOfGuests();
+      this.setAndRemoveDisabled(this.amountOfAdults, this.buttonRemoveAdults);
+      this.setMaxAmount(this.amountOfAdults, this.buttonAddAdults, 16);
+
+      this.outputQuantityOfGuests();
+      this.setAndRemoveDisabled(this.amountOfChildren, this.buttonRemoveChildren);
+      this.setMaxAmount(this.amountOfChildren, this.buttonAddChildren, 16);
+
       this.outputQuantityOfGuests();
       this.setAndRemoveDisabled(this.amountOfBabyes, this.buttonRemoveBabyes);
       this.setMaxAmount(this.amountOfBabyes, this.buttonAddBabyes, 5);
     }
+    if (target == 'button__apply') {
+      console.log('Применить')
+    }
+    this.hideAndShowButtonClear();
   }
   outputQuantityOfGuests() {
     let sumOfGuests = this.amountOfAdults + this.amountOfChildren;
@@ -129,6 +185,16 @@ class DropdownGuests {
       elem.setAttribute('disabled', 'disabled');
     } else if (amount < maxAmount) {
       elem.removeAttribute('disabled')
+    }
+  }
+  hideAndShowButtonClear() {
+    let sumOfGuests = this.amountOfAdults + this.amountOfBabyes + this.amountOfChildren;
+    if (sumOfGuests === 0) {
+      this.buttonClear.classList.remove('button__clear')
+      this.buttonClear.classList.add('button__clear_hidden')
+    } else {
+      this.buttonClear.classList.remove('button__clear_hidden')
+      this.buttonClear.classList.add('button__clear')
     }
   }
 }
