824be899e3d1262519e3c7f3cb038c67d386e009
Viacheslav Kaskov <Dr.Sfar@yandex.ru>
2020-05-07 18:59:29 +0300

    (UI Kit)Сделан filter-date-dropdown и subscription-text-field

---
+++ src/components/__filter-date-dropdown/filter-date-dropdown.js
@@ -0,0 +1,18 @@
+$('.filter-date-airdatepicker').datepicker({
+  minDate: new Date,
+  range: true,
+  clearButton: true,
+  applyButton: true,
+  multipleDatesSeparator: ' - ',
+  dateFormat: 'dd M',
+  onSelect: function(formattedDate, date, inst) {
+    if (formattedDate) {
+    $('.filter-date-dropdown__area-of-placeholder').text(formattedDate)
+    } else {
+      $('.filter-date-dropdown__area-of-placeholder').text('19 авг - 23 авг')
+    }
+  }
+}).data('datepicker')
+$('.filter-date-dropdown__show-info').click(function() {
+  $('.filter-date-airdatepicker').toggle();
+})
--- src/uikit/formelements/formelements.js
+++ src/uikit/formelements/formelements.js
@@ -2,4 +2,5 @@ import './formelements.scss'
 import dropdownGuests from '../../components/__dropdown/dropdown.js';
 
 import '../../components/__air-datepicker/js/datepicker.js';
-import dateDropdown from '../../components/__date-dropdown/date-dropdown';
+import '../../components/__date-dropdown/date-dropdown';
+import '../../components/__filter-date-dropdown/filter-date-dropdown';
