18f0f00b563d52d8ed3a900314b0d35dfd1e2357
Viacheslav Kaskov <Dr.Sfar@yandex.ru>
2020-05-11 17:07:34 +0300

    (UiKit) Сделан like button

---
+++ src/components/__like-button/like-button.js
@@ -0,0 +1,24 @@
+class Like {
+  constructor() {
+    this.toggle = document.querySelector('.like-button__toggle')
+    this.amount = document.querySelector('.like-button__amount')
+  }
+  init() {
+    this.toggle.addEventListener('click', this.liked.bind(this))
+  }
+  liked(e) {
+    e.preventDefault();
+    if (this.toggle.classList.contains('like-active')) {
+      this.toggle.classList.remove('like-active')
+      let reduceAmount = +this.amount.innerHTML - 1;
+      this.amount.innerHTML = reduceAmount;
+    } else {
+      this.toggle.classList.add('like-active');
+      let increaseAmount = +this.amount.innerHTML + 1;
+      this.amount.innerHTML = increaseAmount;
+    }
+  }
+}
+
+let like = new Like;
+like.init();
--- src/uikit/formelements/formelements.js
+++ src/uikit/formelements/formelements.js
@@ -5,3 +5,4 @@ import '../../components/__date-dropdown/date-dropdown';
 import '../../components/__filter-date-dropdown/filter-date-dropdown';
 import '../../components/__dropdown-bedroom/dropdown-bedroom';
 import '../../components/__expandable-checkbox-list/expandabel-checkbox-list'
+import '../../components/__like-button/like-button'
