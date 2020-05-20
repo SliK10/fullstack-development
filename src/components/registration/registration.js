232713ce78619f19e0b6db3f9bd531aa074c8630
Viacheslav Kaskov <Dr.Sfar@yandex.ru>
2020-05-20 17:04:28 +0300

    (UiKit) Cards. Сделан блок регистрации

---
+++ src/components/registration/registration.js
@@ -0,0 +1,1 @@
+import './__masked-text-field/masked-text-field'
--- src/uikit/cards/cards.js
+++ src/uikit/cards/cards.js
@@ -1,2 +1,3 @@
 import './cards.scss'
 import '../../components/search-room/search-room'
+import '../../components/registration/registration'
