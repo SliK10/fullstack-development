855a4baa9380928a8509f82e29ea8bda244e9273
Viacheslav Kaskov <Dr.Sfar@yandex.ru>
2020-06-15 17:19:01 +0300

    (Room details) Подключил ApexChart, не работает скрипт диаграммы. Выскакивает ошибка ApexCharts is not defined

--- src/pages/room-details/room-details.js
+++ src/pages/room-details/room-details.js
@@ -1,55 +1,22 @@
 import './room-details.scss'
 import '../../components/to-book/to-book'
-// import './chart.js/Chart'
+import './apexchart/apexcharts.js'
 
-// var ctx = document.getElementById('myChart').getContext('2d');
+var divChart = document.querySelector("#chart");
+var options = {
+  chart: {
+    height: 120,
+    type: 'bar'
+  },
+  series: [{
+    name: 'sales',
+    data: [30,40,35,50,49,60,70,91,125]
+  }],
+  xaxis: {
+    categories: [1991,1992,1993,1994,1995,1996,1997, 1998,1999]
+  }
+}
 
-// var darkGradient = ctx.createLinearGradient(0, 0, 0, 100);
-// darkGradient.addColorStop(0, '#919191')
-// darkGradient.addColorStop(1, '#3D4975')
+var chart = new ApexCharts(divChart, options);
 
-// var violetGradient = ctx.createLinearGradient(0, 0, 0, 100);
-// violetGradient.addColorStop(0, '#BC9CFF');
-// violetGradient.addColorStop(1, '#8BA4F9');
-
-// var greenGradient = ctx.createLinearGradient(0, 100, 0, 0);
-// greenGradient.addColorStop(0, '#6FCF97');
-// greenGradient.addColorStop(1, '#66D2EA');
-
-// var orangeGradient = ctx.createLinearGradient(0, 0, 0, 100);
-// orangeGradient.addColorStop(0, '#FFE39C');
-// orangeGradient.addColorStop(1, '#FFBA9C');
-
-// var chart = new Chart(ctx, {
-//     type: 'doughnut',
-
-//     // The data for our dataset
-//     data: {
-//         labels: ['Разочарован', 'Хорошо', 'Удовлетворительно', 'Великолепно'],
-//         datasets: [{
-//             label: 'My First dataset',
-//             backgroundColor: [ darkGradient, violetGradient, greenGradient, orangeGradient],
-//             borderColor: '#fff',
-//             borderWidth: 2,
-//             data: [0, 25, 25, 50]
-//         }]
-//     },
-
-//     // Configuration options go here
-//     options: {
-//       cutoutPercentage: 90,
-//       legend: {
-//         position: 'right'
-//       },
-//       elements: {
-//         center: {
-//           text: '260',
-//           color: '#FF6384', // Default is #000000
-//           fontStyle: 'Arial', // Default is Arial
-//           sidePadding: 20, // Default is 20 (as a percentage)
-//           minFontSize: 25, // Default is 20 (in px), set to false and text will not wrap.
-//           lineHeight: 25 // Default is 25 (in px), used for when text wraps
-//         }
-//       }
-//     }
-// });
+chart.render();
