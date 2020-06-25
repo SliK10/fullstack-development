bd4eebeac0a837afce2928470c408c0d6c4a1ad8
Viacheslav Kaskov <Dr.Sfar@yandex.ru>
2020-06-25 17:27:43 +0300

    (to-book) Сделал логику рассчета стоимости номера. Нужно сделать работу скрипта по кнопке Применить

--- src/components/to-book/to-book.js
+++ src/components/to-book/to-book.js
@@ -5,22 +5,60 @@ import moment from './momentjs/moment'
 
 class Calculator {
   constructor() {
-    this.arrival = document.querySelector('.arrival')
-    this.departure = document.querySelector('.departure')
-    this.rangeFrom = document.querySelector("-range-from-")
+    this.toBook = document.querySelector('.to-book')
+    this.costs = this.toBook.querySelector('.price__number')
+    this.costsDescription = this.toBook.querySelector(".information__description")
+    this.informPrice = this.toBook.querySelector('.information__price')
+    this.totalPrice = this.toBook.querySelector('.total__price')
+    this.descriptionPrice = this.toBook.querySelector('.information__description-price')
+    this.serviceCharge = this.toBook.querySelector('.information__price-service')
   }
   init() {
-    this.arrival.addEventListener('click', this.date)
+    this.toBook.addEventListener('click', this.date.bind(this))
   }
   date(e) {
-    console.log(e.target.innerHTML)
-    let a = moment(e.target.innerHTML, "DD.MM.YYYY");
-    console.log(a)
-  }
-  calculate(arrival, departure) {
+    if (e.target.classList.contains('material-icons') ) {
+      let computedStyle = getComputedStyle(document.querySelector('.date-airdatepicker'))
+      if (computedStyle.display === 'none') {
+        let dateArrival = document.querySelector('.arrival');
+        let dateDeparture = document.querySelector('.departure');
 
+        dateArrival = dateArrival.innerHTML.split('.');
+        dateDeparture = dateDeparture.innerHTML.split('.');
+
+        dateArrival.reverse();
+        dateDeparture.reverse();
+
+        let a = moment(dateArrival);
+        let b = moment(dateDeparture);
+
+        let total = b.diff(a, 'days');
+
+        this.informPrice.innerHTML = `${this.calculateTotalCosts(total)}₽`
+        this.totalPrice.innerHTML = `${this.calculateTotalCosts(total) - this.transformToNumber(this.descriptionPrice.innerHTML) + this.transformToNumber(this.serviceCharge.innerHTML)}₽`
+      }
+    }
+  }
+  calculateTotalCosts(total) {
+    let price = this.costs.innerHTML;
+    let priceNumber;
+
+    priceNumber = this.transformToNumber(price);
+
+    return priceNumber * total;
+  }
+  transformToNumber(string) {
+    let stringWithoutSpace = '';
+
+    for (let char of string) {
+      if (char !== ' ') {
+        stringWithoutSpace += char;
+      }
+    }
+    stringWithoutSpace = parseInt(stringWithoutSpace);
+
+    return stringWithoutSpace;
   }
 }
 let calculator = new Calculator;
 calculator.init();
-// console.log(moment().format('MMMM Do YYYY, h:mm:ss a'))
